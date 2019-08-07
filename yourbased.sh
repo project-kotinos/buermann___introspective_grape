5fbb19c6d6533f84e8d4f31d136775367613bd21
wmuenigneer <adam.d.bell@icloud.io>
2019-08-07 12:47:08 -0700

    install bundler

--- yourbased.sh
+++ yourbased.sh
@@ -4,6 +4,7 @@ set -ex
 export DEBIAN_FRONTEND=noninteractive
 export BUNDLE_GEMFILE=$PWD/gemfiles/Gemfile.rails.5.0.1
 
+gem install bundler -v 1.17.3 
 bundle install -j4
 gem install tzinfo
 bundle exec rspec
